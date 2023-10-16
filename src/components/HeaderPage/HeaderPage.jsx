5b19516674bae4c9b3ab48edfdb815657a59d118
Gianfranco Pedrol <tech@provatis.academy>
2023-10-15 22:24:58 -0300

    ajustes registre atendimento

--- src/components/HeaderPage/HeaderPage.jsx
+++ src/components/HeaderPage/HeaderPage.jsx
@@ -3,35 +3,36 @@ import { Box, Text, Image } from "@chakra-ui/react";
 import bgImage from "../../assets/img/bg-img-01.png";
 
 function HeaderPages({ namePage }) {
-  return (
-    <Box
-      m={{ md: "0px", sm: "50px" }}
-      mt={{ md: "20px", sm: "0px" }}
-      display={"flex"}
-      alignItems={"center"}
-      bgColor={"white"}
-      maxWidth={"95%"}
-      height={"150px"}
-      borderRadius={"15px"}
-      boxShadow={"md"}
-      justifyContent={"space-between"}
-      p={8}
-    >
-      <Box width={"70%"}>
-        <Text fontSize={"2xl"} fontWeight={"medium"}>
-          {namePage}
-        </Text>
-      </Box>
-      <Box width={"30%"}>
-        <Box
-          backgroundImage={bgImage}
-          bgSize={"cover"}
-          minHeight={"150px"}
-          minWidth={"400px"}
-        ></Box>
-      </Box>
-    </Box>
-  );
+	return (
+		<Box
+			m={{ md: "0px", sm: "50px" }}
+			mt={{ md: "20px", sm: "0px" }}
+			display={"flex"}
+			alignItems={"center"}
+			bgColor={"white"}
+			maxWidth={"95%"}
+			height={"150px"}
+			borderRadius={"15px"}
+			boxShadow={"md"}
+			justifyContent={"space-between"}
+			p={8}
+		>
+			<Box width={"70%"}>
+				<Text fontSize={"2xl"} fontWeight={"medium"}>
+					{namePage}
+				</Text>
+			</Box>
+			<Box width={"30%"}>
+				<Box
+					backgroundImage={bgImage}
+					bgSize={"cover"}
+					minHeight={"150px"}
+					minWidth={"300px"}
+					maxWidth={"400px"}
+				></Box>
+			</Box>
+		</Box>
+	);
 }
 
 export default HeaderPages;
--- src/components/NewsApointments/NewsApointments.jsx
+++ src/components/NewsApointments/NewsApointments.jsx
@@ -1,77 +1,88 @@
 import React from "react";
-import { Box, Button, Table, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import {
+	Box,
+	Button,
+	Table,
+	Tbody,
+	Td,
+	Th,
+	Thead,
+	Tr,
+	HStack,
+} from "@chakra-ui/react";
 import { FaWhatsapp, FaExternalLinkAlt } from "react-icons/fa";
 
 function NextAppointmentsTable({ appointments }) {
-  function sendConfirmationViaWhatsApp(patientName) {
-    console.log(`Enviando confirmação para ${patientName} via WhatsApp.`);
-  }
+	function sendConfirmationViaWhatsApp(patientName) {
+		console.log(`Enviando confirmação para ${patientName} via WhatsApp.`);
+	}
 
-  function goToAppointmentDetails(appointmentId) {
-    console.log(`Acessando detalhes do atendimento ID ${appointmentId}.`);
-  }
+	function goToAppointmentDetails(appointmentId) {
+		console.log(`Acessando detalhes do atendimento ID ${appointmentId}.`);
+	}
 
-  return (
-    <Box
-      m={{ md: "0px", sm: "50px" }}
-      mt={{ md: "20px", sm: "0px" }}
-      mb={{ md: "40px", sm: "20px" }}
-      display={"flex"}
-      alignItems={"center"}
-      bgColor={"white"}
-      maxWidth={"95%"}
-      borderRadius={"15px"}
-      boxShadow={"md"}
-      justifyContent={"space-between"}
-      p={8}
-      overflowX="auto" // Adicionando overflow horizontal
-    >
-      <Table variant="simple" minW="100%">
-        <Thead>
-          <Tr>
-            <Th>Nome do Paciente</Th>
-            <Th>Profissional</Th>
-            <Th>Serviço</Th>
-            <Th>Valor do Atendimento</Th>
-            <Th>Data Agendada</Th>
-            <Th>Ações</Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {appointments.map((appointment, index) => (
-            <Tr key={index}>
-              <Td>{appointment.patientName}</Td>
-              <Td>{appointment.professionalName}</Td>
-              <Td>{appointment.service}</Td>
-              <Td>{appointment.appointmentValue}</Td>
-              <Td>{appointment.appointmentDate}</Td>
-              <Td>
-                <Button
-                  leftIcon={<FaWhatsapp />}
-                  colorScheme="green"
-                  mr={2}
-                  onClick={() =>
-                    sendConfirmationViaWhatsApp(appointment.patientName)
-                  }
-                >
-                  Confirmar via WhatsApp
-                </Button>
-                <Button
-                  leftIcon={<FaExternalLinkAlt />}
-                  colorScheme="teal"
-                  onClick={() =>
-                    goToAppointmentDetails(appointment.appointmentId)
-                  }
-                >
-                  Detalhes
-                </Button>
-              </Td>
-            </Tr>
-          ))}
-        </Tbody>
-      </Table>
-    </Box>
-  );
+	return (
+		<Box
+			m={{ md: "0px", sm: "50px" }}
+			mt={{ md: "20px", sm: "0px" }}
+			mb={{ md: "40px", sm: "20px" }}
+			display={"flex"}
+			alignItems={"center"}
+			bgColor={"white"}
+			maxWidth={"95%"}
+			borderRadius={"15px"}
+			boxShadow={"md"}
+			justifyContent={"space-between"}
+			p={8}
+			overflowX="auto"
+		>
+			<Table variant="simple" minW="100%">
+				<Thead>
+					<Tr>
+						<Th>Nome do Paciente</Th>
+						<Th>Profissional</Th>
+						<Th>Serviço</Th>
+						<Th>Valor do Atendimento</Th>
+						<Th>Data Agendada</Th>
+						<Th>Ações</Th>
+					</Tr>
+				</Thead>
+				<Tbody>
+					{appointments.map((appointment, index) => (
+						<Tr key={index}>
+							<Td>{appointment.patientName}</Td>
+							<Td>{appointment.professionalName}</Td>
+							<Td>{appointment.service}</Td>
+							<Td>{appointment.appointmentValue}</Td>
+							<Td>{appointment.appointmentDate}</Td>
+							<Td>
+								<HStack spacing={2}>
+									<Button
+										leftIcon={<FaWhatsapp />}
+										colorScheme="green"
+										onClick={() =>
+											sendConfirmationViaWhatsApp(appointment.patientName)
+										}
+									>
+										Enviar Confirmação
+									</Button>
+									<Button
+										leftIcon={<FaExternalLinkAlt />}
+										colorScheme="teal"
+										onClick={() =>
+											goToAppointmentDetails(appointment.appointmentId)
+										}
+									>
+										Detalhes
+									</Button>
+								</HStack>
+							</Td>
+						</Tr>
+					))}
+				</Tbody>
+			</Table>
+		</Box>
+	);
 }
 
 export default NextAppointmentsTable;
